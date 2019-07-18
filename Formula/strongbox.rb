8c80156251169543045d1c9712b2d0b0caab6cba
goreleaserbot <goreleaser@carlosbecker.com>
2019-07-18 18:02:49 +0100

    Brew formula update for strongbox version 0.1.8

--- Formula/strongbox.rb
+++ Formula/strongbox.rb
@@ -2,14 +2,14 @@
 class Strongbox < Formula
   desc "Securely store secrets at rest using Hashicorp Vault"
   homepage "https://github.com/mvisonneau/strongbox"
-  version "0.1.7"
+  version "0.1.8"
 
   if OS.mac?
-    url "https://github.com/mvisonneau/strongbox/releases/download/0.1.7/strongbox_0.1.7_darwin_amd64.tar.gz"
-    sha256 "871ed3563c0b047113e14f2d56ade70482241da29f98085fe1f6ab73b3059a45"
+    url "https://github.com/mvisonneau/strongbox/releases/download/0.1.8/strongbox_0.1.8_darwin_amd64.tar.gz"
+    sha256 "298d70d1ba31100689a365b156f2e32e962fc38af39a44404692fad220e50abf"
   elsif OS.linux?
-    url "https://github.com/mvisonneau/strongbox/releases/download/0.1.7/strongbox_0.1.7_linux_amd64.tar.gz"
-    sha256 "7d5d8b48734a36983d6443983e8a5814736d1e01a114d8cc2dae86074bfd58c7"
+    url "https://github.com/mvisonneau/strongbox/releases/download/0.1.8/strongbox_0.1.8_linux_amd64.tar.gz"
+    sha256 "a491acfcabe09b58a7df854e4d1c4039dc97300b0cf66f5472fec6cfdd5ae1c0"
   end
 
   def install
