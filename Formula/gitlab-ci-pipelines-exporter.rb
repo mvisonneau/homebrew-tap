655fb372ea31fbf7c8730bc212bd3c937f52829b
goreleaserbot <goreleaser@carlosbecker.com>
2020-04-30 17:44:05 +0100

    Brew formula update for gitlab-ci-pipelines-exporter version 0.3.1

--- Formula/gitlab-ci-pipelines-exporter.rb
+++ Formula/gitlab-ci-pipelines-exporter.rb
@@ -2,21 +2,21 @@
 class GitlabCiPipelinesExporter < Formula
   desc "GitLab CI pipelines exporter (prometheus/open-metrics)"
   homepage "https://github.com/mvisonneau/gitlab-ci-pipelines-exporter"
-  version "0.3.0"
+  version "0.3.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/mvisonneau/gitlab-ci-pipelines-exporter/releases/download/0.3.0/gitlab-ci-pipelines-exporter_0.3.0_darwin_amd64.tar.gz"
-    sha256 "ef4b5026f43b3c89623f600ca51169c7537c08ecc1de03e1cd1e68006d578b31"
+    url "https://github.com/mvisonneau/gitlab-ci-pipelines-exporter/releases/download/0.3.1/gitlab-ci-pipelines-exporter_0.3.1_darwin_amd64.tar.gz"
+    sha256 "2ae61b20a66ffd570f9226f286b85d5df4ebb663450724fda68420d88ad5011f"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/mvisonneau/gitlab-ci-pipelines-exporter/releases/download/0.3.0/gitlab-ci-pipelines-exporter_0.3.0_linux_amd64.tar.gz"
-      sha256 "746350e8ed1464e69651199b142575c3c50a950656ca1abbd302347601631099"
+      url "https://github.com/mvisonneau/gitlab-ci-pipelines-exporter/releases/download/0.3.1/gitlab-ci-pipelines-exporter_0.3.1_linux_amd64.tar.gz"
+      sha256 "246404767b6162d2db2388c04eed59b48390bfe0ff47f5edb08495f8793a2db7"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/mvisonneau/gitlab-ci-pipelines-exporter/releases/download/0.3.0/gitlab-ci-pipelines-exporter_0.3.0_linux_arm64.tar.gz"
-        sha256 "a623abb861d8f89b8082758c1bd22a87e40777a269698c114389e23d6df830f4"
+        url "https://github.com/mvisonneau/gitlab-ci-pipelines-exporter/releases/download/0.3.1/gitlab-ci-pipelines-exporter_0.3.1_linux_arm64.tar.gz"
+        sha256 "3f9aace397fdd039faea215e973974a47a07a599843a01c9ca98653b87782e61"
       else
       end
     end
