c5aae10e067063723e18ac1695152e4657b44582
goreleaserbot <goreleaser@carlosbecker.com>
2020-10-22 20:27:52 +0100

    Brew formula update for vac version 0.0.4

--- Formula/vac.rb
+++ Formula/vac.rb
@@ -2,21 +2,21 @@
 class Vac < Formula
   desc "VAC - Vault AWS Credentials Manager"
   homepage "https://github.com/mvisonneau/vac"
-  version "0.0.3"
+  version "0.0.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/mvisonneau/vac/releases/download/0.0.3/vac_0.0.3_darwin_amd64.tar.gz"
-    sha256 "8b9f3a7a7ba82d9d8779a81e9fc9c775b09c86e2d72ce3840b8bfff8adf7ce8c"
+    url "https://github.com/mvisonneau/vac/releases/download/0.0.4/vac_0.0.4_darwin_amd64.tar.gz"
+    sha256 "933a05931f4ca92738d76e8410d2314f8f1e5b8814b8e15ed9a741947e80d9de"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/mvisonneau/vac/releases/download/0.0.3/vac_0.0.3_linux_amd64.tar.gz"
-      sha256 "8588ae6c1c2f719404a07e5f9f3057d7f636b61477f83700dc5c7d3c6edf9d29"
+      url "https://github.com/mvisonneau/vac/releases/download/0.0.4/vac_0.0.4_linux_amd64.tar.gz"
+      sha256 "d5689cacffa462b732114c9f4e7e2b41f6a32d1ae30da22ce0610bfd4c6f5662"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/mvisonneau/vac/releases/download/0.0.3/vac_0.0.3_linux_arm64.tar.gz"
-        sha256 "042f97ce694cad871f4954497e75560f11accc8d928c04f23137774b8143516e"
+        url "https://github.com/mvisonneau/vac/releases/download/0.0.4/vac_0.0.4_linux_arm64.tar.gz"
+        sha256 "8668712bfded4f009cc0645f423d49197bb358ae04bef8570dc32189bd9b83fa"
       else
       end
     end
