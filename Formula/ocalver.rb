5b13f1b977c052e9b7242ca5db15fae942f3155f
goreleaserbot <goreleaser@carlosbecker.com>
2022-02-11 13:22:33 +0000

    Brew formula update for ocalver version v0.0.6

--- Formula/ocalver.rb
+++ Formula/ocalver.rb
@@ -5,27 +5,43 @@
 class Ocalver < Formula
   desc "Opinionated CalVer generator"
   homepage "https://github.com/mvisonneau/ocalver"
-  version "0.0.5"
-  bottle :unneeded
+  version "0.0.6"
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/mvisonneau/ocalver/releases/download/v0.0.5/ocalver_v0.0.5_darwin_amd64.tar.gz"
-    sha256 "b10cff88ca056823a241c0b74776efa5e060ad76ec56ba1b6e958161f2c48023"
-  end
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/mvisonneau/ocalver/releases/download/v0.0.5/ocalver_v0.0.5_darwin_arm64.tar.gz"
-    sha256 "abc4b5b886024fbaaea9de9edf4158833249e8dce3077fecc7b0482647f96d4c"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/mvisonneau/ocalver/releases/download/v0.0.5/ocalver_v0.0.5_linux_amd64.tar.gz"
-    sha256 "18aedefa72344478add7191d990fa195957a1f58fe8741c52708b84a92a28664"
-  end
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/mvisonneau/ocalver/releases/download/v0.0.5/ocalver_v0.0.5_linux_arm64.tar.gz"
-    sha256 "cf6ebd6992e2760d6e99349f0c588da7f09c17432f407833809ba8acb0cfd35e"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/mvisonneau/ocalver/releases/download/v0.0.6/ocalver_v0.0.6_darwin_amd64.tar.gz"
+      sha256 "7eda3d02a762bbd693ffa7cd4a4574a0203f479b1be54f27cd426681b5c4033d"
+
+      def install
+        bin.install "ocalver"
+      end
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/mvisonneau/ocalver/releases/download/v0.0.6/ocalver_v0.0.6_darwin_arm64.tar.gz"
+      sha256 "47a425849272fda8f42dabc421dbc5b281194a23c35bdaede77c1dab65223466"
+
+      def install
+        bin.install "ocalver"
+      end
+    end
   end
 
-  def install
-    bin.install "ocalver"
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/mvisonneau/ocalver/releases/download/v0.0.6/ocalver_v0.0.6_linux_amd64.tar.gz"
+      sha256 "aa4c28217ac6fac43534aa20d76fa7ce5d46d09ce0680155eebc870e49cc94c7"
+
+      def install
+        bin.install "ocalver"
+      end
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/mvisonneau/ocalver/releases/download/v0.0.6/ocalver_v0.0.6_linux_arm64.tar.gz"
+      sha256 "16fc9df337c0a247942cbd80258d7c9d040326ac87bba0ff02b7705e88b4f7b6"
+
+      def install
+        bin.install "ocalver"
+      end
+    end
   end
 end
