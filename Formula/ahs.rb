36dee3134eee37f58aec062f62b94fad6f4d526b
goreleaserbot <goreleaser@carlosbecker.com>
2022-02-11 13:54:03 +0000

    Brew formula update for ahs version v0.2.7

--- Formula/ahs.rb
+++ Formula/ahs.rb
@@ -5,21 +5,39 @@
 class Ahs < Formula
   desc "Amazon EC2 Hostname Setter"
   homepage "https://github.com/mvisonneau/ahs"
-  version "0.2.6"
-  depends_on :linux
+  version "0.2.7"
 
-  on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/mvisonneau/ahs/releases/download/v0.2.6/ahs_0.2.6_linux_arm64.tar.gz"
-      sha256 "30f09e796baa5c36e13384e080a482d00119709b28af622cc4ab04e9be7c5ae8"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/mvisonneau/ahs/releases/download/v0.2.7/ahs_0.2.7_darwin_arm64.tar.gz"
+      sha256 "db11d3bc8b648dc9c8a11df1644e8b7a416d48af68d2dbb5d6fc7e1a7aa3074a"
 
       def install
         bin.install "ahs"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/mvisonneau/ahs/releases/download/v0.2.6/ahs_0.2.6_linux_amd64.tar.gz"
-      sha256 "8cb0797e3f3bbf9755c0bd7a668de3add52328765cede21f4fe86ecf9ad07716"
+      url "https://github.com/mvisonneau/ahs/releases/download/v0.2.7/ahs_0.2.7_darwin_amd64.tar.gz"
+      sha256 "140d713e526a954756396463914cd3e01f09ea4c557907a2651f7130569450aa"
+
+      def install
+        bin.install "ahs"
+      end
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/mvisonneau/ahs/releases/download/v0.2.7/ahs_0.2.7_linux_arm64.tar.gz"
+      sha256 "e9c4b7258468cafbc1b6f1b3de10f9293651b305bfb74f2f513d75eae729f94f"
+
+      def install
+        bin.install "ahs"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/mvisonneau/ahs/releases/download/v0.2.7/ahs_0.2.7_linux_amd64.tar.gz"
+      sha256 "958258388770518f990ab6cdf9d818fab9d55f15c14606fad3f87c0be54aca52"
 
       def install
         bin.install "ahs"
